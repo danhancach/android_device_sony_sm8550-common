b6633627f1b20f14b151407be4cbfc14d3938d72
danhancach <111163507+danhancach@users.noreply.github.com>
2026-09-16 19:44:02 +0900

    sm8550-common: Tune powerhint for sustained perf and SoMC camera
    Ship a device-owned powerhint.xml with Moto-style sustained/camera
    caps, Sony SoMC camera hints, and fixed PSM Target attribute matching.

--- common.mk
+++ common.mk
@@ -314,7 +314,7 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
-    vendor/qcom/opensource/power/config/kalama/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 $(call soong_config_set,qtipower,mode_ext_lib,//$(LOCAL_PATH):libpowermode-ext-sony)
 
